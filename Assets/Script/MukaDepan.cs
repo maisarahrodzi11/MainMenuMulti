681de67cd2edfa3701df470f2b47ae36df80d373
maisarahrodzi <maisarahrodzi11@gmail.com>
2024-01-07 19:30:51 +0800

    credit scene, main menu

--- Assets/Script/MukaDepan.cs
+++ Assets/Script/MukaDepan.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MukaDepan : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
    public void Play()
     {
